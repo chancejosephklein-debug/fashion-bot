9ab29de6c2c21e3f99edb411922aabf2df2baaba
chancejosephklein-debug <chancejosephklein@icloud.com>
2026-09-24 20:33:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 TREND_CHANNEL_ID = int(os.getenv("TREND_CHANNEL_ID", 0))
 ALERT_ROLE_ID = int(os.getenv("ALERT_ROLE_ID", 0))
-CHECK_INTERVAL_HOURS = int(os.getenv("CHECK_INTERVAL_HOURS", 24))
 
-# Social Trends API (Free tier available)
-SOCIAL_TRENDS_BASE = "https://tiktok-scraper.omkar.cloud"
-TIKTOK_MARKET = "us" # United States region
-
-# StockX Scraper API (Apify)
 APIFY_TOKEN = os.getenv("APIFY_TOKEN")
-STOCKX_ACTOR_ID = "crawlerbros~stockx-scraper"
+
+TIKTOK_ACTOR = "apidojo/tiktok-scraper-api"
+STOCKX_ACTOR = "xtracto/stockx-search-scraper"
+GRAILED_ACTOR = "jongoose/grailed-scraper"
+
 APIFY_BASE = "https://api.apify.com/v2"
 
-# Your watchlist for trend scanning
+TIMEZONE = "America/Los_Angeles"
+LOCATION_LABEL = "Yuba City, CA"
+
 BRANDS = [
     "Hellstar", "Chrome Hearts", "Supreme", "Bape", "Stussy",
     "Denim Tears", "Corteiz", "Trapstar", "Essentials Fear of God",
@@ -22,18 +22,8 @@ BRANDS = [
     "Sp5der", "Revenge", "Gallery Dept", "Rhude", "Eric Emanuel",
     "Syna World", "Nocta", "Nike Tech Fleece",
     "Nike Dunk", "Jordan 1", "Jordan 4", "Yeezy", "New Balance 550",
-    "New Balance 9060", "Asics Gel", "Salomon", "Samba", "Adidas Samba",
-    "Air Force 1", "Air Max 1", "Air Max 95", "Nike SB", "Dunk Low",
-    "Dunk High", "Jordan 11", "Air Max 97",
-    "Rick Owens", "Balenciaga", "Maison Margiela", "Comme des Garcons",
-    "CdG Play", "Stone Island", "Moncler", "Vivienne Westwood",
-    "Diesel", "True Religion", "Evisu", "Ralph Lauren",
-    "Tommy Hilfiger", "Carhartt", "Carhartt WIP",
-    "Travis Scott", "Fragment", "Sacai", "KAWS", "Vlone",
-    "Anti Social Social Club", "Golf Wang", "FTP", "Dime",
-    "Pleasures", "Kith", "Palace", "Fear of God",
-    "Aelfric Eden", "Cole Buxton", "Vuja De", "Entire Studios",
+    "Asics Gel", "Salomon", "Samba", "Air Force 1", "Air Max 1",
+    "Rick Owens", "Balenciaga", "Stone Island", "Moncler",
+    "Diesel", "True Religion", "Evisu", "Ralph Lauren", "Carhartt",
+    "Travis Scott", "Fragment", "KAWS", "Vlone", "Kith", "Palace",
 ]
-
-TIMEZONE = "America/Los_Angeles"
-LOCATION_LABEL = "Yuba City, CA"
